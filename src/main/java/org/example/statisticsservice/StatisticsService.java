3d0c015f8d7a713788aaaffc01958c5334bb6708
Daniel_Canillas <Daniel_Canillas@epam.com>
2022-02-21 13:49:44 +0100

    authentication

--- src/main/java/org/example/statisticsservice/StatisticsService.java
+++ src/main/java/org/example/statisticsservice/StatisticsService.java
@@ -3,9 +3,11 @@ package org.example.statisticsservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EntityScan(basePackages = "org.example.modelproject")
 public class StatisticsService {
 
 	@Bean
--- src/main/java/org/example/statisticsservice/controller/LogController.java
+++ src/main/java/org/example/statisticsservice/controller/LogController.java
@@ -4,6 +4,7 @@ import org.example.modelproject.dto.LogMongoDTO;
 import org.example.statisticsservice.service.impl.LogServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
@@ -21,18 +22,21 @@ public class LogController {
 
     // get all logs
     @GetMapping("")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<LogMongoDTO>> getAllLogs(){
         return ResponseEntity.ok().body(consumerService.getAllLogs());
     }
 
     // get by id
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<LogMongoDTO> getLogById(@PathVariable(value = "id") long id){
         return ResponseEntity.ok().body(consumerService.getLogById(id));
     }
 
     // get by date
     @GetMapping("/date/{from}/{to}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<LogMongoDTO>> getLogsByDate(@PathVariable(value = "from") Date from,
                                                   @PathVariable(value = "to") Date to){
         return ResponseEntity.ok().body(consumerService.getLogsByDate(from, to));
@@ -40,7 +44,8 @@ public class LogController {
 
     // create log
     @PostMapping("")
-    public ResponseEntity<LogMongoDTO> createCustomer(@RequestBody LogMongoDTO logMongoDTO){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<LogMongoDTO> createLog(@RequestBody LogMongoDTO logMongoDTO){
         return ResponseEntity.ok().body(consumerService.saveLog(logMongoDTO));
     }
 }
